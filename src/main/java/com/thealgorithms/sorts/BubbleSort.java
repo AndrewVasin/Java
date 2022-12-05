b0c447b81cc5016d147e7d70aaa9d0c00a77b3dc
AndrewVasin <91669078+AndrewVasin@users.noreply.github.com>
2022-12-05 10:22:03 +0300

    update bubble sort
    Updated the algorithm to reduce its running time. After each operation of the inner loop, the largest element is located on the right, which in the future can not be compared with the rest. Therefore, we can shift the border on the right by one - the speed of work increases by eliminating unnecessary comparisons.

--- src/main/java/com/thealgorithms/sorts/BubbleSort.java
+++ src/main/java/com/thealgorithms/sorts/BubbleSort.java
@@ -18,9 +18,10 @@ class BubbleSort implements SortAlgorithm {
      */
     @Override
     public <T extends Comparable<T>> T[] sort(T[] array) {
+        int level = array.length - 1;
         for (int i = 1, size = array.length; i < size; ++i) {
             boolean swapped = false;
-            for (int j = 0; j < size - i; ++j) {
+            for (int j = 0; j < level; ++j) {
                 if (greater(array[j], array[j + 1])) {
                     swap(array, j, j + 1);
                     swapped = true;
@@ -29,6 +30,7 @@ class BubbleSort implements SortAlgorithm {
             if (!swapped) {
                 break;
             }
+            level--;
         }
         return array;
     }
